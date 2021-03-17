9dace4a1ae14d7ccbe7f2593fd9f4d134c8242d5
Pluto505 <plutoject@gmail.com>
2021-03-17 18:55:48 +0200

    applied fmt

--- data/scripts/talk/100063.js
+++ data/scripts/talk/100063.js
@@ -1,37 +1,39 @@
-var ScriptBridge = Java.type('com.steelteam.openrhynn.scripting.ScriptBridge');
-var ChanceCalculator = Java.type('com.steelteam.openrhynn.utilits.ChanceCalculator');
+var ScriptBridge = Java.type('com.steelteam.openrhynn.scripting.ScriptBridge')
+var ChanceCalculator = Java.type(
+  'com.steelteam.openrhynn.utilits.ChanceCalculator'
+)
 
-var getExecutionTime = function() { return 15000; }
+var getExecutionTime = function () {
+  return 15000
+}
 
-var onTalkRequest = function(world, mob) {
-	var message = 'Nothing to say...';
-	var chance = 20;
-	var target = world.getEntity(mob.aiFollow);	
-	
-	if(target != null) {
-		message = 'I kill you, ' + target.name + '!';
-		chance = 60;
-	} else {
-		switch(ScriptBridge.sharedRandom.nextInt(5)) {
-			case 0:
-				message = 'I\'LL ERASE YOU IN POWDER!';
-				break;
-			case 1:
-				message = 'Disappear!!!';
-				break;
-			case 2:
-				message = 'Pitiful mortal';
-				break;
-			case 3:
-				message = 'I will break your bones!';
-				break;
-			case 4:
-				message = 'Another pathetic adventurer!';
-				break;
-		}
-	}
-	
-	if(ChanceCalculator.calculate(chance))
-		world.chat(mob.objectId, message);
-	
+var onTalkRequest = function (world, mob) {
+  var message = 'Nothing to say...'
+  var chance = 20
+  var target = world.getEntity(mob.aiFollow)
+
+  if (target != null) {
+    message = 'I kill you, ' + target.name + '!'
+    chance = 60
+  } else {
+    switch (ScriptBridge.sharedRandom.nextInt(5)) {
+      case 0:
+        message = "I'LL ERASE YOU IN POWDER!"
+        break
+      case 1:
+        message = 'Disappear!!!'
+        break
+      case 2:
+        message = 'Pitiful mortal'
+        break
+      case 3:
+        message = 'I will break your bones!'
+        break
+      case 4:
+        message = 'Another pathetic adventurer!'
+        break
+    }
+  }
+
+  if (ChanceCalculator.calculate(chance)) world.chat(mob.objectId, message)
 }
